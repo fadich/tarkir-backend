eb368630fcfd15d0ec3b08357fd5a9b8b6363729
Fadi Ahmad <fadi.ahmad@aspectum.com>
2021-02-25 19:02:07 +0200

    API v2 implementation

--- src/spellbook/api/run.py
+++ src/spellbook/api/run.py
@@ -2,8 +2,6 @@ from tarkir.api import app
 
 from spellbook.api.views import (
     ColorsView,
-    SpellsView,
-    SchoolsView,
     SchoolTreeView,
 )
 
@@ -15,14 +13,6 @@ app.add_url_rule(
 app.add_url_rule(
     '/colors',
     view_func=ColorsView.as_view(ColorsView.__name__)
-)
-app.add_url_rule(
-    '/schools',
-    view_func=SchoolsView.as_view(SchoolsView.__name__)
-)
-app.add_url_rule(
-    '/spells',
-    view_func=SpellsView.as_view(SpellsView.__name__)
 )
 
 
--- src/spellbook/api/views.py
+++ src/spellbook/api/views.py
@@ -1,10 +1,8 @@
 from tarkir.api.views import ModelListView
 
-from spellbook.models import Color, School, Spell
+from spellbook.models import Color, School
 from spellbook.schemas import (
     ColorSchema,
-    SpellSchema,
-    SchoolSchema,
     SchoolTreeSchema,
 )
 
@@ -13,17 +11,22 @@ class SchoolTreeView(ModelListView):
     schema = SchoolTreeSchema()
     model = School
 
+    def get(self):
+        school_tree = super().get()
+
+        return list(map(self.reformat_school, school_tree))
+
+    @classmethod
+    def reformat_school(cls, school: dict):
+        return {
+            **school,
+            'spells': {
+                f'{school["shortcut"]}{spell["cycle"]}': spell
+                for spell in school['spells']
+            }
+        }
+
 
 class ColorsView(ModelListView):
     schema = ColorSchema()
     model = Color
-
-
-class SchoolsView(ModelListView):
-    schema = SchoolSchema()
-    model = School
-
-
-class SpellsView(ModelListView):
-    schema = SpellSchema()
-    model = Spell
