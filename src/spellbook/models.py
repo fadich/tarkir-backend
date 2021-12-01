5e24fdc7c94d2aac65bfcf25884c0c0a416e8ccf
Fadi Ahmad <royalfadich@gmail.com>
2021-12-01 17:47:52 +0200

    Added Passive Bonus relations

--- src/spellbook/models.py
+++ src/spellbook/models.py
@@ -10,6 +10,77 @@ __all__ = [
 from tarkir_base.database import db, Model
 
 
+class Color(Model):
+    __tablename__ = 'color'
+
+    # pylint: disable=invalid-name
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(256), nullable=False, unique=True)
+    shortcut = db.Column(db.String(1), nullable=False, unique=True)
+    hex_code = db.Column(db.String(16), nullable=False)
+
+    schools = db.relationship('School', back_populates='color')
+    spells = db.relationship('SpellToColor', back_populates='color')
+
+    def __repr__(self):
+        return f'{self.shortcut}'
+
+
+class School(Model):
+    __tablename__ = 'school'
+
+    # pylint: disable=invalid-name
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(256), nullable=False, unique=True)
+    shortcut = db.Column(db.String(2), nullable=False, unique=True)
+    color_id = db.Column(
+        db.Integer(), db.ForeignKey('color.id'), nullable=False
+    )
+    description = db.Column(db.Text(), nullable=True)
+
+    color = db.relationship('Color', back_populates='schools')
+    spells = db.relationship('SpellToSchool', back_populates='school')
+    passive_bonuses = db.relationship('PassiveBonusToSchool', back_populates='school')
+
+    def __repr__(self):
+        return f'{self.shortcut}'
+
+
+class Spell(Model):
+    __tablename__ = 'spell'
+
+    # pylint: disable=invalid-name
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(256), nullable=False, unique=True)
+    name_en = db.Column(db.String(256), nullable=True, unique=True)
+    type = db.Column(db.String(1024), nullable=True)
+    description = db.Column(db.Text(), nullable=True)
+    requirements = db.Column(db.Text(), nullable=True)
+    time_to_create = db.Column(db.String(1024), nullable=True)
+    cost = db.Column(db.String(1024), nullable=True)
+    duration = db.Column(db.String(1024), nullable=True)
+    items = db.Column(db.Text(), nullable=True)
+
+    colors = db.relationship('SpellToColor', back_populates='spell')
+    schools = db.relationship('SpellToSchool', back_populates='spell')
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class PassiveBonus(Model):
+    __tablename__ = 'passive_bonus'
+
+    # pylint: disable=invalid-name
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(256), nullable=False, unique=True)
+    name_en = db.Column(db.String(256), nullable=True, unique=True)
+    description = db.Column(db.Text(), nullable=True)
+    cycle = db.Column(db.Integer(), nullable=False, default=lambda: 0)
+
+    schools = db.relationship('PassiveBonusToSchool', back_populates='passive_bonus')
+
+
 class SpellToColor(Model):
     __tablename__ = 'spell_to_color'
 
@@ -49,67 +120,17 @@ class SpellToSchool(Model):
         return f'{self.school.shortcut}-{self.spell.name}'
 
 
-class Color(Model):
-    __tablename__ = 'color'
+class PassiveBonusToSchool(Model):
+    __tablename__ = 'passive_bonus_to_school'
 
-    # pylint: disable=invalid-name
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String(256), nullable=False, unique=True)
-    shortcut = db.Column(db.String(1), nullable=False, unique=True)
-    hex_code = db.Column(db.String(16), nullable=False)
-
-    schools = db.relationship('School', back_populates='color')
-    spells = db.relationship('SpellToColor', back_populates='color')
-
-    def __repr__(self):
-        return f'{self.shortcut}'
-
-
-class School(Model):
-    __tablename__ = 'school'
-
-    # pylint: disable=invalid-name
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String(256), nullable=False, unique=True)
-    shortcut = db.Column(db.String(2), nullable=False, unique=True)
-    color_id = db.Column(
-        db.Integer(), db.ForeignKey('color.id'), nullable=False
+    passive_bonus_id = db.Column(
+        db.Integer, db.ForeignKey('passive_bonus.id'), primary_key=True,
+        on_delete='CASCADE'
     )
-    description = db.Column(db.Text(), nullable=True)
+    school_id = db.Column(
+        db.Integer, db.ForeignKey('school.id'), primary_key=True,
+        on_delete='CASCADE'
+    )
 
-    color = db.relationship('Color', back_populates='schools')
-    spells = db.relationship('SpellToSchool', back_populates='school')
-
-    def __repr__(self):
-        return f'{self.shortcut}'
-
-
-class Spell(Model):
-    __tablename__ = 'spell'
-
-    # pylint: disable=invalid-name
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    name = db.Column(db.String(256), nullable=False, unique=True)
-    name_en = db.Column(db.String(256), nullable=True, unique=True)
-    type = db.Column(db.String(1024), nullable=True)
-    description = db.Column(db.Text(), nullable=True)
-    requirements = db.Column(db.Text(), nullable=True)
-    time_to_create = db.Column(db.String(1024), nullable=True)
-    cost = db.Column(db.String(1024), nullable=True)
-    duration = db.Column(db.String(1024), nullable=True)
-    items = db.Column(db.Text(), nullable=True)
-
-    colors = db.relationship('SpellToColor', back_populates='spell')
-    schools = db.relationship('SpellToSchool', back_populates='spell')
-
-    def __repr__(self):
-        return f'{self.name}'
-
-
-# TODO: Add School level bonus (passives) models
-class PassiveBonus:
-    pass
-
-
-class PassiveBonusToSchool:
-    pass
+    passive_bonus = db.relationship('PassiveBonus', back_populates='schools')
+    school = db.relationship('School', back_populates='passive_bonuses')
