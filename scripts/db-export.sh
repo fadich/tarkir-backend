3784c9fa01f01ac9c558e50e1eda0d87e350b525
Fadi Ahmad <royalfadich@gmail.com>
2021-12-03 21:21:07 +0200

    Feature/tk 25 setup backup database mechanism (#28)
    * db-export.sh updated
    
    * db-export.sh updated
    
    * db-export.sh updated
    
    * db-export.sh updated
    
    * db-export.sh updated

--- scripts/db-export.sh
+++ scripts/db-export.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if [ -z "${1}" ]
+then
+    DUMP_PATH_HOST="./dump-$(date +'%Y-%m-%d-%H-%M-%S').sql"
+else
+    DUMP_PATH_HOST="${1}"
+fi
+
+echo ${DUMP_PATH_HOST}
 
 DB_NAME='postgres'
 CONTAINER_NAME='tarkir-db'
-#POSTGRES_HOME='/var/lib/postgresql'
-DUMP_FILE_PATH="dump-$(date +'%Y-%m-%d-%H-%M-%S').sql"
-DUMP_PATH_CONTAINER="/tmp/${DUMP_FILE_PATH}"
-DUMP_PATH_HOST="${SCRIPT_DIR}/resources/${DUMP_FILE_PATH}"
+DUMP_PATH_CONTAINER="/tmp/dump-$(date +'%Y-%m-%d-%H-%M-%S').sql"
 
 echo "Create and export new SQL-dump..."
-docker-compose exec ${CONTAINER_NAME} bash -c \
+docker-compose exec -T ${CONTAINER_NAME} bash -c \
   "su -c \"pg_dump --clean -Z 0 -d ${DB_NAME} \
   | sed -e '/^--/d' \
   | sed -e '/^$/d' \
   > ${DUMP_PATH_CONTAINER}\" postgres"
 
+echo "Extracting dump file from container..."
 container_id=$(docker-compose ps -q ${CONTAINER_NAME})
-docker cp "${container_id}":"${DUMP_PATH_CONTAINER}" "${DUMP_PATH_HOST}"
+docker cp "${container_id}":"${DUMP_PATH_CONTAINER}" "${DUMP_PATH_HOST}" || exit 1
 
 echo "Done and stored to ${DUMP_PATH_HOST}"
