baac618d04a3ef878656c366ae9ac3fabd00ceb4
Fadi Ahmad <royalfadich@gmail.com>
2021-12-09 16:22:36 +0200

    Feature/tk 20 setup wsgi and nginx for backend (#44)
    * uWSGI installing
    
    * uWSGI installing

--- src/spellbook/application.py
+++ src/spellbook/application.py
@@ -3,9 +3,9 @@ from tarkir_base.api import app, admin_app
 from spellbook.api import v1, v2
 from spellbook.admin.views import *
 
-app.register_blueprint(v1.blueprint, url_prefix='/') # TODO: remove it after design update
-app.register_blueprint(v1.blueprint, url_prefix='/v1')
-app.register_blueprint(v2.blueprint, url_prefix='/v2')
+app.register_blueprint(v1.blueprint, url_prefix='/')  # TODO: remove it after design update
+app.register_blueprint(v1.blueprint, url_prefix='/api/v1/')
+app.register_blueprint(v2.blueprint, url_prefix='/api/v2/')
 
 app.init_admin(
     admin=admin_app,
@@ -23,4 +23,5 @@ app.init_admin(
     )
 )
 
-app.run()
+if __name__ == '__main__':
+    app.run()
